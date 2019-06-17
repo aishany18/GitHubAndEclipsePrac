23c43b73c9f76ae36d4d96e84e07112038e8636c
AISHA MALIK <37249178+aishany18@users.noreply.github.com>
2019-06-17 16:23:50 -0400

    Add files via upload

---
+++ GitHub.java
@@ -0,0 +1,11 @@
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Save it in GitHub");
+		System.out.println("Import it from Git Hub then");
+
+	}
+
+}
